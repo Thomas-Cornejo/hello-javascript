89d0e438f26f4103b36d12eaafcdc280e2e13eb7
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2024-12-04 12:50:36 -0500

    add exercises

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,16 +4,53 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
+//Esto es un comentario
 
 // 2. Escribe un comentario en varias líneas
 
+/*
+Estos son varios 
+comentarios
+en varias lineas
+*/
+
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+// Tipos de datos primitivos
+
+let name = 'Thomas'
+let age = 21
+let ImStudent = true
+let Undefined
+let nullValue = null
+let mySimbol = Symbol("mysymbol")
+let myBigInt = BigInt(1312412412412412412412)
+
 
 // 4. Imprime por consola el valor de todas las variables
 
+
+console.log(name)
+console.log(age)
+console.log(ImStudent)
+console.log(Undefined)
+console.log(nullValue)
+console.log(mySimbol)
+console.log(myBigInt)
+
 // 5. Imprime por consola el tipo de todas las variables
 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof ImStudent)
+console.log(typeof Undefined)
+console.log(typeof nullValue)
+console.log(typeof mySimbol)
+console.log(typeof myBigInt)
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
